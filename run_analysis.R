b2ed4048957f70d7cf96fd56b063a7c1233ff47c
Scott Lane <scottdlane@gmail.com>
2014-11-22 23:08:05 -0500

    Version 1.0 of run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,17 +4,35 @@ run_analysis <- function() {
     ## 2.) Extracts only the measurements on the mean and standard deviation
     ##      for each Measurement.
     
-    ## Part 1.) Merge the training and test sets to create one data set
+    ## This R script, called run_analysis.R, does the following:
+    ## 1.) Merges the training and the test sets to create one data set.
+    ## 2.) Extracts only the measurements on the mean and standard deviation for each 
+    ##      measurement. 
+    ## 3.) Uses descriptive activity names to name the activities in the data set.
+    ## 4.) Appropriately labels the data set with descriptive variable names. 
+    ## 5.) From the data set in step 4, creates a second, independent tidy data set with the 
+    ##      average of each variable for each activity and each subject.
     
+    ## This R script performs these steps in the order described above.
+
+    ## Before the frist step can be performed, the working directory is set, the data is 
+    ## downloaded from the data resposity, the data is unzipped in place, and the data
+    ## files are read into memory for the R program to use.
+
+    ## This R script creates a data frame, called aggr, that follows the data design
+    ## structure described here: 
+    ## https://class.coursera.org/getdata-009/forum/thread?thread_id=58#comment-369
+        
     ## Set the working directory
     setwd("~/datasciencecoursera/Getting_and_Cleaning_Data/project")
     
-    ## First, collect all of the needed data
     ## Create a subdirectory for the data, if none exists
     if (!file.exists("./data")) {dir.create("./data")}
+    
+    ## Assign the URL to the location of the data 
     fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
     
-    ## Download the zip file, if it doesn't already exist
+    ## Download and time stamp the zip file, if it doesn't already exist
     if (!file.exists("./data/Dataset.zip")) {
         download.file(fileURL, destfile="./data/Dataset.zip", method="curl", temp)
         dateDownloaded <- date()
@@ -36,65 +54,79 @@ run_analysis <- function() {
     activity_labels <- read.table("data/UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)
     
     
+    ## =================================================================================
     ## STEP 1
-    ## Merge the training and the test sets to create one data set.
+    ## 1.) Merge the training and the test sets to create one data set.
     
-    ## Aggregate all training and test data together into a master data.frame 
-    ## called "aggr". 
-    ## Bind all of the train and test rows together
+    ## Aggregate all training and test data together into a data.frame.
+    ## Bind all of the train and test rows together using several rbind commands
     aggr <- rbind(X_train, X_test)
     subject_aggr <- rbind(subject_train, subject_test)
     y_aggr <- rbind(y_train, y_test)
     
-    ## Bind all of the columns together
+    ## Bind all of the columns togetherusing several cbind commands
     aggr <- cbind(aggr, subject_aggr)
     aggr <- cbind(aggr, y_aggr)
     
+    ## The result of STEP 1 is a 10299 x 563 data frame, called aggr.
     
+    
+    ## =================================================================================
     ## STEP 2
     ## Extract only the measurements on the mean and standard deviation for 
     ## each measurement.
     
     ## We first need to know which columns contain mean and standard deviation
-    ## data. Let's go ahead and lable the subject and activity columns 
+    ## data. For this project, we are assuming that all variables that contain the string
+    ## "mean" are mean variables and variables that contain the string "std" are standard
+    ## deviation variables. We also label the subject and activity columns 
     ## appropriately as well.
     
-    ## First, I need to remove the redundant column of data in features
+    ## First, remove the redundant column of data in features
     features <- subset(features, select = -c(1) )
        
+    ## Set the aggr column names to the labels in features.txt    
     features_array <- c(do.call("cbind", features))
     colnames(aggr) <- features_array
     
-    ## Label the last to columns "subject" and "activity"
+    ## Label the last two columns "subject" and "activity"
     colnames(aggr)[ncol(aggr)-1] <- c("subject")   
     colnames(aggr)[ncol(aggr)] <- c("activity")
 
-    ## Now, include all columns that are only related to mean and standard 
-    ## deviation.
+    ## Now include all columns that are only related to mean and standard 
+    ## deviation
     select_columns <- grepl("mean|std", features_array)
     aggr <- aggr[select_columns == TRUE]
     
+    ## The result of STEP 2 is a 10299 x 81 data frame, called aggr, that contains
+    ## only the mean and standard deviation data.
     
     
+    ## ================================================================================   
     ## STEP 3
     ## Use descriptive activity names to name the activities in the data set. 
     
-    ## The activity names that we want to use are in activity_labels. 
-    ## Remove the redundant column.
+    ## The activity names that we want to use are in activity_labels.txt.
+    ## Remove the redundant column
     activity_labels <- subset(activity_labels, select = -c(1) )
     
-    ## Replace the number in the activity column with the matching activity 
-    ## description in activity_labels.
+    ## Replace the number in the activity column with the matching activity. 
+    ## description in activity_labels. We also reduce the text to lower case to better
+    ## match the variable text.
     activity_labels_array <- c(do.call("cbind",activity_labels)) 
     activity_labels_array <- tolower(activity_labels_array)
     for (i in 1:nrow(aggr)) {aggr$activity[i] <- activity_labels_array[as.integer(aggr$activity[i])]}
  
+    ## The result of STEP 3 is a 10299 x 81 data frame, called aggr, with descriptive 
+    ## activity names.
     
+    
+    ## ================================================================================   
     ## STEP 4
     ## Appropriately label the data set with descriptive variable names.  
     
     ## The data set aggr already has descriptive variable names; however, I will
-    ## improve them a bit for readability
+    ## improve them a bit for readability.
     ## Remove the parenthesis in the variable names
     names(aggr) <- gsub("\\(", "", names(aggr),)    
     names(aggr) <- gsub("\\)", "", names(aggr),)
@@ -113,18 +145,21 @@ run_analysis <- function() {
     # Save the column names of the reduced data set for later use
     select_names <- names(aggr)
     
+    ## The result of STEP 4 is a 10299 x 81 data frame, called aggr, with cleaned and
+    ## descriptive variable names.     
     
-    
+       
+    ## ================================================================================   
     ## STEP 5
     ## From the data set in step 4, create a second, independent tidy data 
     ## set with the average of each variable for each activity and each subject.
     
-    ## Create a new tidy data set from the original
-    
-    ## Need to sort aggr and the activities list first
+    ## Sort aggr and activities list first. This ensures that we are collecting the 
+    ## correct raw data as we calculate the mean for each subject and activity.
     sorted_aggr <- arrange(aggr, subject, activity)
     sorted_activities <- sort(activity_labels_array)
     
+    ## Create a new tidy data set from the original
     aggr_mean = data.frame()
     for (i in 1:30) {
         for (j in seq_along(sorted_activities)) {
@@ -138,34 +173,13 @@ run_analysis <- function() {
     ## Set column names to new data set
     colnames(aggr_mean) <- c("subject","activity", select_names[1:(size-2)])
     
+    ## The result of STEP 5 is a 180 x 81 data frame, called aggr_mean, with that contains
+    ## a mean of each variable for each activity and subject.
+
+
+    ## ================================================================================   
+
+    ## Write the aggr_mean data frame out to a text file for posting to Coursera
     write.table(aggr_mean, file = "aggr_mean.txt", row.names = FALSE) 
-    
-#     
-# #     Step 0: read all the files : listed along with dimensions
-# #     
-# #     features.txt # 561x2
-# #     X_train.txt  # 7352x561
-#     X_test.txt  # 2947x561
-#     subject_train.txt # 7352x1
-#     subject_test.txt # 2947x1
-#     Y_train.txt # 7532x1
-#     Y_test.txt # 2947x1
-#     
-#     1. rbind xtrain and xtest  # 10299x561
-#     
-#     2. filter mean cols # 10299x46
-#     
-#     3. set table headers using features 
-#     
-#     5. a) rbind subject train and subject test, and set the subject col names # 10299x1
-# 
-# b) rbind ytrain with ytest, and set the activity col names  # 10299x1
-# 
-# c) cbind xtrain mean cols from 2  with ytrain /test from 5b) .   # 10299x47
-# 
-# cbind the result with subject trains/test from 5a)    # 10299x48
-# 
-# 4. replace the activity with readable name. (Yes! I did 4 after 5)
-# 
 
 }
